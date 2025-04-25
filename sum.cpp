d1e27c05241151b62a141e941885d152bf4cafac
LianeAmain <amainliane@gmail.com>
2025-04-25 00:06:20 -0300

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double number1;
+    double number2;
+    cout<<"Enter first number:";
+    cin>>number1;
+    cout<<"Enter second number:";
+    cin>>number2;
+    double sum=number1+number2;
+    cout<<"The sum is:"<<sum<<endl;
+
+    return 0;
+
+
+}
