435b20a25e366d3499864d38e0250bf3a1691d17
LianeAmain <amainliane@gmail.com>
2025-04-25 00:07:50 -0300

    Add files via upload

---
+++ geboortedatum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int huidigejaar=2025;
+    int geboortejaar;
+    cout<<"geboortejaar:";
+    cin >>geboortejaar;
+    int leeftijd=huidigejaar-geboortejaar;
+    cout<<"U bent "<<leeftijd<<" jaar oud"<<endl;
+     return 0;
+}
